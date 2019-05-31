fb41a69986849599f8c363d358ad43f5a46342d4
Joseph Blume <blumejr26@gmail.com>
2019-05-31 15:20:07 -0400

    added functionality to stats servlet and added stats.html file

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -7,13 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/* Responds with a hard-coded message for testing purposes */
+import com.google.codeu.data.Datastore;
+import com.google.gson.JsonObject;
+
+/* Handles fetching site statistics */
 
 @WebServlet("/stats")
 public class StatsPageServlet extends HttpServlet{
 
+    private Datastore datastore;
+
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-            response.getOutputStream().println("hello world");
+    public void init(){
+        datastore = new Datastore();
+    }
+
+    /* Responds with site statistics in JSON */
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+
+        response.setContentType("application/json");
+
+        int messageCount = datastore.getTotalMessageCount();
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("messageCount", messageCount);
+        response.getOutputStream().println(jsonObject.toString());
     }
 }
