c30bcf2ba2d8f383bb42966befb083aa0c470848
UrviA <urviagrawal99@gmail.com>
2019-06-18 01:16:59 -0400

    Display map and list all restaurants on the results page

--- src/main/webapp/js/results-loader.js
+++ src/main/webapp/js/results-loader.js
@@ -1,4 +1,4 @@
-function createMarker(map, lat, lng, name, address, zipcode) {
+function createRestaurantMarker(map, lat, lng, name, address, zipcode) {
   const marker = new google.maps.Marker({
     position: {lat: lat, lng: lng},
     map: map,
@@ -13,25 +13,54 @@ function createMarker(map, lat, lng, name, address, zipcode) {
   });
 }
 
-function createRestaurantsMap(restaurants) {
-//  fetch('/restaurant-data').then(function(response) {
-//    return response.json();
-//  }).then((restaurants) => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.4428, lng: -79.943},
-      zoom: 11
-    });
-    restaurants.forEach((restaurant) => {
-      createMarker(map, restaurant.lat, restaurant.lng, restaurant.name, restaurant.address, restaurant.zipcode);
-    });
-//  });
+function buildRestaurantDiv(restaurant) {
+  const nameDiv = document.createElement('div');
+  nameDiv.classList.add('restaurant-name');
+  nameDiv.appendChild(document.createTextNode(restaurant.name));
+
+  const addressDiv = document.createElement('div');
+  addressDiv.classList.add('restaurant-address');
+  addressDiv.innerHTML = restaurant.address+', '+restaurant.zipcode.toString(10);
+  
+  const categoryDiv = document.createElement('div');
+  categoryDiv.classList.add('restaurant-category');
+  categoryDiv.innerHTML = restaurant.category;
+    
+  const restaurantDiv = document.createElement('div');
+  restaurantDiv.classList.add('restaurant-div');
+  restaurantDiv.appendChild(nameDiv);
+  restaurantDiv.appendChild(addressDiv);
+  restaurantDiv.appendChild(categoryDiv);
+
+  return restaurantDiv;
 }
 
 function loadResults() {
   fetch('/restaurant-data').then(function(response) {
     return response.json();
   }).then((restaurants) => {
-      createRestaurantsMap(restaurants);
+    // Create a map
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.4428, lng: -79.943},
+      zoom: 11
+    });
+    
+    const resultsContainer = document.getElementById('results-container');
+    if (restaurants.length == 0)
+    if (restaurants.length == 0) {
+      resultsContainer.innerHTML = '<p>There are no restaurants matching this criteria.</p>';
+    } else {
+      resultsContainer.innerHTML = '';
+    }
+    
+    restaurants.forEach((restaurant) => {
+      // Create a marker for each restaurant on the map
+      createRestaurantMarker(map, restaurant.lat, restaurant.lng, restaurant.name, restaurant.address, restaurant.zipcode);
+      
+      //List each restaurant
+      const restaurantDiv = buildRestaurantDiv(restaurant);
+      resultsContainer.appendChild(restaurantDiv);
+    });
   });
 }
 
