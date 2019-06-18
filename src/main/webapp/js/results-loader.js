d94a821864ccacc127ee6a8ff59af41bf228b2cf
UrviA <urviagrawal99@gmail.com>
2019-06-18 01:22:55 -0400

    Refactored some js code

--- src/main/webapp/js/results-loader.js
+++ src/main/webapp/js/results-loader.js
@@ -13,6 +13,29 @@ function createRestaurantMarker(map, lat, lng, name, address, zipcode) {
   });
 }
 
+function fetchRestaurants(map) {
+  fetch('/restaurant-data').then(function(response) {
+    return response.json();
+  }).then((restaurants) => {
+    const resultsContainer = document.getElementById('results-container');
+    if (restaurants.length == 0)
+    if (restaurants.length == 0) {
+      resultsContainer.innerHTML = '<p>There are no restaurants matching this criteria.</p>';
+    } else {
+      resultsContainer.innerHTML = '';
+    }
+    
+    restaurants.forEach((restaurant) => {
+      // Create a marker for each restaurant on the map
+      createRestaurantMarker(map, restaurant.lat, restaurant.lng, restaurant.name, restaurant.address, restaurant.zipcode);
+      
+      //List each restaurant
+      const restaurantDiv = buildRestaurantDiv(restaurant);
+      resultsContainer.appendChild(restaurantDiv);
+    });
+  });
+}
+
 function buildRestaurantDiv(restaurant) {
   const nameDiv = document.createElement('div');
   nameDiv.classList.add('restaurant-name');
@@ -36,32 +59,14 @@ function buildRestaurantDiv(restaurant) {
 }
 
 function loadResults() {
-  fetch('/restaurant-data').then(function(response) {
-    return response.json();
-  }).then((restaurants) => {
-    // Create a map
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.4428, lng: -79.943},
-      zoom: 11
-    });
-    
-    const resultsContainer = document.getElementById('results-container');
-    if (restaurants.length == 0)
-    if (restaurants.length == 0) {
-      resultsContainer.innerHTML = '<p>There are no restaurants matching this criteria.</p>';
-    } else {
-      resultsContainer.innerHTML = '';
-    }
-    
-    restaurants.forEach((restaurant) => {
-      // Create a marker for each restaurant on the map
-      createRestaurantMarker(map, restaurant.lat, restaurant.lng, restaurant.name, restaurant.address, restaurant.zipcode);
-      
-      //List each restaurant
-      const restaurantDiv = buildRestaurantDiv(restaurant);
-      resultsContainer.appendChild(restaurantDiv);
-    });
+  // Create a map
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.4428, lng: -79.943},
+    zoom: 11
   });
+
+  // Display all results - on map and as a list
+  fetchRestaurants(map);
 }
 
 
