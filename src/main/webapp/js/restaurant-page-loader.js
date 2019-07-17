f2dfb27032fbf9dfb0c5b3985784203f5640c898
UrviA <urviagrawal99@gmail.com>
2019-07-16 21:41:43 -0400

    more updates to generic restaurant page

--- src/main/webapp/js/restaurant-page-loader.js
+++ src/main/webapp/js/restaurant-page-loader.js
@@ -5,6 +5,12 @@ function loadRestaurantData() {
   fetch('/restaurant?name='+name).then(function(response) {
     return response.json();
   }).then((restaurant) => {
-    console.log(restaurant);
+    if (restaurant === '') {
+      document.getElementById('name').innerHTML = "404: we don't recognize this location :(";
+    }
+    else {
+      document.getElementById('name').innerHTML = restaurant.name;
+      document.getElementById('address').innerHTML = restaurant.address + ', ' + restaurant.zipcode.toString();
+    }
   })
 }
