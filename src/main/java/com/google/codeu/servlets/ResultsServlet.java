da2aa6c0dab197a06591f066360d737107d6e624
UrviA <urviagrawal99@gmail.com>
2019-07-16 20:58:27 -0400

    cleaned up results servlet file

--- src/main/java/com/google/codeu/servlets/ResultsServlet.java
+++ src/main/java/com/google/codeu/servlets/ResultsServlet.java
@@ -12,9 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.List;
-import java.lang.Math;
 
 /**
  * Returns Restaurant data as a JSON array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
@@ -37,20 +35,6 @@ public class ResultsServlet extends HttpServlet {
     String json = gson.toJson(restaurants);
     response.getWriter().println(json);
     
-  }
-    
-  private double distance(double lat1, double lon1, double lat2, double lon2) {
-    if ((lat1 == lat2) && (lon1 == lon2)) {
-      return 0;
-    }
-    else {
-      double theta = lon1 - lon2;
-      double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) *                           Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-      dist = Math.acos(dist);
-      dist = Math.toDegrees(dist);
-      dist = dist * 60 * 1.1515;
-      return (dist);
-    }
   }
 
 }
