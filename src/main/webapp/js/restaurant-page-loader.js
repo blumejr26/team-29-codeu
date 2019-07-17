1df701bb513d34e190d0a48a00c1e9052bb0cf64
UrviA <urviagrawal99@gmail.com>
2019-07-16 22:12:05 -0400

    added fetching restaurant reviews on restaurant page

--- src/main/webapp/js/restaurant-page-loader.js
+++ src/main/webapp/js/restaurant-page-loader.js
@@ -11,6 +11,30 @@ function loadRestaurantData() {
     else {
       document.getElementById('name').innerHTML = restaurant.name;
       document.getElementById('address').innerHTML = restaurant.address + ', ' + restaurant.zipcode.toString();
+      document.getElementById('deals').innerHTML = "Deals: ";
+      document.getElementById('reviews').innerHTML = "Reviews: ";
+      fetch('/reviews?restaurant='+restaurant.name).then(function(response) {
+        return response.json();
+      }).then((reviews) => {
+        const reviewsDiv = document.createElement('div');
+        if (reviews.length === 0) {
+          reviewsDiv.innerHTML = 'There are no reviews for this restaurant!';
+        }
+        else {
+          reviews.forEach((review) => {
+            const userDiv = document.createElement('div');
+            userDiv.innerHTML = review.user;
+            const timeDiv = document.createElement('div');
+            timeDiv.appendChild(document.createTextNode(new Date(review.timestamp)));
+            const textDiv = document.createElement('div');
+            textDiv.innerHTML = review.text;
+            reviewsDiv.appendChild(userDiv);
+            reviewsDiv.appendChild(userDiv);
+            reviewsDiv.appendChild(userDiv);
+          });
+        }
+        document.getElementById('reviews').appendChild(reviewsDiv);
+      })
     }
-  })
+  });
 }
